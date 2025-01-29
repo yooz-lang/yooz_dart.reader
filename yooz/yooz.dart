d2c63271b1a06044f92f7a6a660b20d2530ed590
Mr.Salari <154739444+Salari-info@users.noreply.github.com>
2025-01-29 03:39:53 -0500

    Update yooz.dart

--- yooz/yooz.dart
+++ yooz/yooz.dart
@@ -1,299 +1,226 @@
-// Author : t.me/Salari_info
 library yooz;
 
+import 'dart:io';
 import 'dart:math';
 
-class YouzParser {
-  String input_data_string;
-  String textError = "متاسفم، متوجه نشدم.";
-  List<Map<String, dynamic>> patterns = [];
-  Map<String, String> definitions = {};
-  List<String> stopWords = [];
-  List<String> randomMessage = [];
-  List<Map<String, dynamic>> keywords = [];
-  Map<String, dynamic> tempVars = {};
-  Map<String, dynamic> collections = {};
-  Map<String, dynamic> collection_patterns = {};
-  Map<String, dynamic> lastMatchedPattern;
+class Parser {
+  final List<Map<String, dynamic>> patterns = [];
+  final Map<String, String> variables = {};
+  final Random _random = Random();
+  List spantext = [];
+  String Subject = ''; 
+  String Subjectname = ''; 
+  String lastMessage = ''; 
+  String TextError = 'متاسفم، نمی‌توانم پاسخ دهم.'; 
+  List pronouns = []; 
+  List Verbs = [];
+  bool Bpronouns = false;
+  bool bVerbs = false;
+  Map fixanswer = {}; 
+  final List<Map> addtonextword = []; // add text in end text
 
-  void parse(String input) {
-    discoverCollections(input);
-    input_data_string = input;
-    final definitionRegex = RegExp(r'#(\S+)\s*:\s*(.*?)\s*\.', multiLine: true);
-    for (final match in definitionRegex.allMatches(input)) {
-      final key = match.group(1)?.trim();
-      final value = match.group(2)?.trim();
-      if (key != null && value != null) {
-        definitions[key] = value;
-      }
-    }
+  void loadPatterns(String patternString) {
+    final lines = patternString.split('\n');
+    String? currentPattern;
+    List<String>? currentResponses;
 
-    final patternRegex =
-        RegExp(r'\(\s*\+\s*(.*?)\s*-\s*(.*?)\s*\)', multiLine: true);
-    for (final match in patternRegex.allMatches(input)) {
-      final userPattern = match.group(1)?.trim();
-      final bot = match.group(2)?.trim();
-      String botmsg;
-      if (bot.contains('ـ')){
-        List<String> parts = bot.split('ـ');
-        for (String part in parts) {
-          
-          randomMessage.add(part);
-          var random = Random();
-          int randomIndex = random.nextInt(randomMessage.length);
-          botmsg = randomMessage[randomIndex];
-        }
-      }else {
-        botmsg = bot;
-      }
-
-      var botResponses = [botmsg];
-
-      if (userPattern != null && botResponses != null) {
-        if (userPattern.startsWith('{')) {
-          final keywords = userPattern
-              .substring(1, userPattern.length - 1)
-              .split('،')
-              .map((keyword) => keyword.trim())
-              .toList();
-          this
-              .keywords
-              .add({'keywords': keywords, 'botResponses': botResponses});
-        } else {
-          patterns
-              .add({'userPattern': userPattern, 'botResponses': botResponses});
-        }
-      }
-    }
-
-    final stopWordsRegex = RegExp(r'-\s*\{\s*(.*?)\s*\}', multiLine: true);
-    for (final match in stopWordsRegex.allMatches(input)) {
-      final words =
-          match.group(1)?.split('،').map((word) => word.trim()).toList();
-      if (words != null) {
-        stopWords.addAll(words);
-      }
-    }
-  }
-
-  
-
-  String getResponse(String userMessage) {
-    final cleanedMessage = removeStopWords(userMessage);
-    lastMatchedPattern = null;
-
-    for (final pattern in patterns) {
-      final userPattern = pattern['userPattern'] as String;
-      final botResponses = pattern['botResponses'] as List<String>;
-      final regexPattern = createRegex(userPattern);
-      RegExpMatch matchReg = regexPattern.firstMatch(cleanedMessage);
-      if (matchReg != null) {
-        List<String> match = [matchReg.group(0), matchReg.group(1)];
-        lastMatchedPattern = {
-          'userPattern': userPattern,
-          'botResponses': botResponses
-        };
-        final responses = botResponses;
-        final response =
-            responses[DateTime.now().millisecondsSinceEpoch % responses.length];
-        if (response.endsWith('!>')) {
-          final additionalResponse = getAdditionalResponses(
-              response.substring(0, response.length - 99).trim(),
-              cleanedMessage);
-          return additionalResponse.replaceAll('!>', '');
-        }
-        return resolveResponse(response, match);
-      }
-    }
-
-    final messageWords = cleanedMessage.split(' ');
-    for (final keywordPattern in keywords) {
-      final keywords = keywordPattern['keywords'] as List<String>;
-      final botResponses = keywordPattern['botResponses'] as List<String>;
-      if (containsKeywords(messageWords, keywords)) {
-        lastMatchedPattern = {
-          'keywords': keywords,
-          'botResponses': botResponses
-        };
-        final response = botResponses[
-            DateTime.now().millisecondsSinceEpoch % botResponses.length];
-        if (response.endsWith('!>')) {
-          return getAdditionalResponses(
-              response.substring(0, response.length - 99).trim(),
-              cleanedMessage);
-        }
-        return resolveResponse(response, null);
-      }
-    }
-
-    return textError;
-  }
-
-  String removeStopWords(String message) {
-    final words = message.split(' ');
-    return words.where((word) => !stopWords.contains(word)).join(' ');
-  }
-
-  RegExp createRegex(String pattern) {
-    return (RegExp(
-        '^${pattern.replaceAll(RegExp(r'\\*([0-9]*)'), '(.*?)')}\$'));
-  }
-
-  String resolveResponse(String response, List<String> match) {
-    String resolvedResponse = response;
-    for (int i = 1; i < match.length; i++) {
-      resolvedResponse = resolvedResponse.replaceAll('*$i', match[i].trim());
-    }
-    return resolvedResponse.replaceAllMapped(RegExp(r'#(\S+)'), (match) {
-      return this.definitions[match.group(1)] ?? match.group(0);
-    });
-  }
-
-  bool containsKeywords(List<String> messageWords, List<String> keywords) {
-    return keywords.every((keyword) => messageWords.contains(keyword));
-  }
-
-  double random() {
-    return Random().nextDouble();
-  }
-
-  String getAdditionalResponses(String initialResponse, String userMessage) {
-    String additionalResponses = initialResponse;
-    for (var pattern in this.patterns) {
-      String userPattern = pattern['userPattern'];
-      final botResponses = pattern['botResponses'];
-      RegExp regexPattern = createRegex(userPattern);
-      RegExpMatch matchReg = regexPattern.firstMatch(userMessage);
-      if (matchReg != null) {
-        List<String> match = [for (int i = 0; i <= matchReg.groupCount; i++) matchReg.group(i)];
-        print(match);
-  
-        final responses = botResponses.cast<String>();
-        final randomResponse = responses[random];
-        additionalResponses += ' ' + resolveResponse(randomResponse, match);
-      }
-    }
-    return additionalResponses;
-  }
-  
-
-  bool _isTempVarDeclarationLine(String line) {
-    line = line.trim();
-    final wordsSeparated = line.split(' ');
-    final firstWord = wordsSeparated[0];
-    final firstChar = firstWord[0];
-    final nextWord = wordsSeparated[1];
-    return firstChar == '=' && nextWord == ':';
-  }
-
-  void defineTempVars(String text) {
-    final lines = text.split('\n');
     for (var line in lines) {
       line = line.trim();
-      final isDeclarationLine = _isTempVarDeclarationLine(line);
-      if (isDeclarationLine) {
-        final chunks = line.split(' ');
-        final firstChunk = chunks[0];
-        final lastChunk = chunks[2];
-        final varName = firstChunk.substring(1);
-        final value = lastChunk.trim();
-        this.tempVars[varName] = value;
-      }
-    }
-  }
 
-  String replaceTempVars(String response) {
-    final lines = response.split('\n');
-    String resultText = '';
-    for (var line in lines) {
-      line = line.trim();
-      final isDeclarationLine = _isTempVarDeclarationLine(line);
-      if (!isDeclarationLine) {
-        final chunks = line.split(' ');
-        for (var chunk in chunks) {
-          final firstChar = chunk[0];
-          if (firstChar == '=') {
-            final varName = chunk.substring(1);
-            resultText += this.tempVars[varName] + ' ';
-          } else {
-            resultText += chunk + ' ';
+      // The next word method
+      if(line.contains('[') && line.contains(']') && line.contains('{') && line.contains('}')){
+        final RegExp regex = RegExp(r'\{(.*?)\}');
+        final matches = regex.allMatches(line);
+        List<Map<String, String>> results = [];
+        for (var match in matches) {
+          String content = match.group(1)!.trim();
+          final numberRegex = RegExp(r'\[(.*?)\]');
+          final numberMatch = numberRegex.firstMatch(content);
+          String number = numberMatch != null ? numberMatch.group(1)!.trim() : '';
+          String output = content.replaceAll(RegExp(r'\[.*?\]'), '');
+          output = output.replaceAll(RegExp(r'\s+'), ' ').trim();
+          List<String> parts = output.split(' > ');
+           if (parts.length == 2) {
+            addtonextword.add({
+              'number':number,
+              'text': parts[0],
+              'end': parts[1],
+            });
           }
         }
       }
-      resultText += '\n';
-    }
-    //console.log(result_text);
-    return resultText;
-  }
-
-  bool _isAnswerPart(String line) {
-    final firstChar = line[0];
-    return firstChar == '-';
-  }
-
-  void discoverCollections(String input) {
-    bool openParenthesis = false;
-    String outsideText = '';
-    int parenthesisDepth = 0;
-  
-    
-    for (int i = 0; i < input.length; i++) {
-      String char = input[i];
-      if (char == '(') {
-        openParenthesis = true;
-        parenthesisDepth++;
-      } else if (char == ')') {
-        parenthesisDepth--;
-        if (parenthesisDepth == 0) {
-          openParenthesis = false;
-          continue;
+      // Save variables method
+      if (line.startsWith('#')) {
+        final parts = line.substring(1).split(':');
+        if (parts.length == 2) {
+          variables[parts[0].trim()] = parts[1].trim();
         }
       }
-
-      if (openParenthesis) {
-        continue;
-      } else {
-        outsideText += char;
+      // Fixed response method
+      if (line.startsWith('{') && line.contains("}") && line.contains('->')) {
+        var a = line.replaceAll('{', '').replaceAll('}', '').split('->');
+        fixanswer[a[0]] = a[1];
+      }
+      // The subject save method
+      if (line.startsWith('=') && line.contains(':')) {
+        final parts = line.substring(1).split(':');
+        if (parts.length == 2) {
+          Subject = parts[1].trim();
+        }
+      }
+      // Text method after each answer
+      if (line.startsWith('+') && line.contains('(') && line.contains(')')) {
+        lastMessage = line
+            .replaceAll('(', '')
+            .replaceAll(')', '')
+            .replaceAll('+', '')
+            .trim();
+      }
+      // The method of identifying the words of Asafa
+      if (line.startsWith('-') && line.contains('{') && line.contains('}')) {
+        spantext = line
+            .replaceAll('}', '')
+            .replaceAll('{', '')
+            .replaceAll('-', '')
+            .trim()
+            .split('،');
+      }
+      if (!line.startsWith('-') && line.endsWith('}') && line.contains('{')) {
+        if (line.startsWith('ضمایر')) {
+          final parts = line.split('{');
+          final key = parts[0].trim();
+          final valuesString = parts[1].replaceAll('}', '').trim();
+          final values = valuesString.split('،').map((s) => s.trim()).toList();
+          if (key == 'ضمایر') {
+            Bpronouns = true;
+            pronouns.addAll(values);
+          } else if (line.startsWith('افعال')) {
+            final parts = line.split('{');
+            final key = parts[0].trim();
+            final valuesString = parts[1].replaceAll('}', '').trim();
+            final values =
+                valuesString.split('،').map((s) => s.trim()).toList();
+            if (key == 'افعال') {
+              bVerbs = true;
+              Verbs.addAll(values);
+            }
+          }
+        }
+      }
+      if (line.startsWith('+')) {
+        if (currentPattern != null && currentResponses != null) {
+          patterns
+              .add({'pattern': currentPattern, 'responses': currentResponses});
+        }
+        currentPattern = line.substring(1).trim();
+        currentResponses = [];
+      }
+      // Random answer method
+      if (line.startsWith('-')) {
+        if (line.contains(Subjectname)) {
+          line = line.replaceAll('=', '').replaceAll('موضوع', Subject);
+        }
+        var responses =
+            line.substring(1).trim().split('_').map((s) => s.trim()).toList();
+        currentResponses?.addAll(responses);
       }
     }
 
-    List<String> lines = outsideText.trim().split('\n');
-
-    for (String line in lines) {
-      if (line.contains('{')) {
-        int startIndex = line.indexOf('{');
-        int endIndex = line.indexOf('}');
-        String between = line.substring(startIndex + 1, endIndex);
-        List<String> items = between.split('،');
-        for (int i = 0; i < items.length; i++) {
-          items[i] = items[i].trim();
-        }
-        String collectionName = line.substring(0, startIndex).trim();
-        this.collections[collectionName] = items;
-      }
+    if (currentPattern != null && currentResponses != null) {
+      patterns.add({'pattern': currentPattern, 'responses': currentResponses});
     }
   }
 
-  String checkForCollectionsPattern(String messageText) {
-    List<String> chunks = messageText.trim().split(' ');
-    Map<String, List<String>> collectionEntries = this.collections;
-    String resultText = '';
-  
-    for (String chunk in chunks) {
-      String isInCollections = '';
-      for (String key in collectionEntries.keys) {
-        if (key.contains(chunk)) {
-          isInCollections = key;
-          break;
-        }
-      }
-      if (isInCollections.isNotEmpty) {
-        resultText += '&' + isInCollections + ' ';
-      } else {
-        resultText += chunk + ' ';
+  String parse(String input) {
+
+    // check and change text in input
+    for (var fix in input.split(' ')) {
+      if (fixanswer.containsKey(fix)) {
+        input = input.replaceAll(fix[0], fix[1]);
       }
     }
-  
-    return resultText.trim();
+
+    // check and add text in end input
+    for (var Eadd in addtonextword) {
+     if (Eadd['text'] == input && num.parse(Eadd['number']) > 0.5) {
+       input = input +' '+ Eadd['end'];
+     }
+    }
+
+    // The method of removing adjectives
+    for (var word in spantext) {
+      input = input.replaceAll(RegExp('\\b${RegExp.escape(word)}\\b'), '');
+    }
+    input = input.replaceAll(RegExp(r'\s+'), ' ').trim();
+
+    // examining variables
+    for (var entry in patterns) {
+      final pattern = entry['pattern'] as String;
+      var responses = entry['responses'] as List<String>;
+      for (var test in responses) {
+        test = test.replaceAll('#', '');
+        if (variables.containsKey(test)) {
+          return variables[test]! + " " + lastMessage;
+      }
+      }
+    }
+
+    // Examining pronouns
+    if (bVerbs || Bpronouns) {
+      for (var pronoun in pronouns) {
+        if (input.contains(pronoun)) {
+          return 'شما از ضمایر استفاده کردین\n' + lastMessage;
+        }
+      }
+      for (var verbs in Verbs) {
+        if (input.contains(verbs)) {
+          return 'شما از افعال استفاده کردین\n' + lastMessage;
+        }
+      }
+    }
+
+    for (var entry in patterns) {
+      final pattern = entry['pattern'] as String;
+      final responses = entry['responses'] as List<String>;
+      final regex = _createRegex(pattern);
+      final match = regex.firstMatch(input);
+      if (match != null) {
+        return _generateResponse(responses, match);
+      }
+    }
+
+    return TextError;
+  }
+
+  RegExp _createRegex(String pattern) {
+    final escapedPattern = RegExp.escape(pattern).replaceAll(r'\*', r'([^ ]+)');
+    return RegExp('^$escapedPattern\$');
+  }
+
+  String _generateResponse(List<String> responses, RegExpMatch match) {
+    // Choose a random answer
+    final response = responses[_random.nextInt(responses.length)];
+    String generatedResponse = response;
+    for (var i = 0; i < match.groupCount; i++) {
+      generatedResponse =
+          generatedResponse.replaceAll('*', match.group(i + 1)!);
+    }
+
+    if (generatedResponse.endsWith('!>')) {
+
+      generatedResponse =
+          generatedResponse.substring(0, generatedResponse.length - 2).trim();
+
+      
+      List<String> variableKey1 =
+          match.group(0)!.split(" "); 
+      for (var variableKey in variableKey1) {
+        if (variableKey != null && variables.containsKey(variableKey)) {
+          generatedResponse +=
+              " " + variables[variableKey]!;
+        }
+      }
+    }
+    return generatedResponse + "\n" + lastMessage;
   }
 }
